65489be089fb90384eda8583cb7b1926fe51b21a
Chris Martin <cj@cjmart.in>
2014-06-01 21:55:59 -0700

    require nfc correctly

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var nfc = require('nfc'),
+var nfc = require('nfc').nfc,
     mifare = require('mifare-classic'),
     ndef = require('ndef');
 
