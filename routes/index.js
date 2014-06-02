44f96f09f970ec4753298482a87ef101c1843406
Chris Martin <cj@cjmart.in>
2014-06-01 19:58:29 -0700

    logic

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+	mifare= require('mifare-classic'),
+	ndef = require('ndef'),
+	router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+	mifare.read(function(err, buffer) {
+		if (err) {
+			console.log("Read failed ");
+			console.log(err);
+		} else {
+			// TODO handle empty buffer!
+			var message = ndef.decodeMessage(buffer.toJSON());
+			console.log("Found NDEF message with " + message.length +
+				(message.length === 1 ? " record" : " records" ));
+			console.log(ndef.stringify(message));            
+		}
+	})
+	
+	res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
