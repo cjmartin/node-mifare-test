9d997cc519a2399b837cd49fa52ae827b443bb1c
Chris Martin <cj@cjmart.in>
2014-06-01 21:22:24 -0700

    try writing first

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,41 @@
 var express = require('express'),
 	mifare= require('mifare-classic'),
 	ndef = require('ndef'),
-	router = express.Router();
+	router = express.Router(),
+	message,
+	bytes;
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	mifare.read(function(err, buffer) {
+	message = [
+		ndef.uriRecord("http://nfc-tools.org"),
+		ndef.textRecord("Hello from nodejs"),
+		ndef.emptyRecord()
+	];
+
+	bytes = ndef.encodeMessage(message);
+
+	mifare.write(bytes, function(err) {
 		if (err) {
-			console.log("Read failed ");
+			console.log("Write failed ");
 			console.log(err);
 		} else {
-			// TODO handle empty buffer!
-			var message = ndef.decodeMessage(buffer.toJSON());
-			console.log("Found NDEF message with " + message.length +
-				(message.length === 1 ? " record" : " records" ));
-			console.log(ndef.stringify(message));            
+			console.log("Write OK");
+
+			mifare.read(function(err, buffer) {
+				if (err) {
+					console.log("Read failed ");
+					console.log(err);
+				} else {
+					// TODO handle empty buffer!
+					var message = ndef.decodeMessage(buffer.toJSON());
+					console.log("Found NDEF message with " + message.length +
+						(message.length === 1 ? " record" : " records" ));
+					console.log(ndef.stringify(message));            
+				}
+			});
 		}
-	})
+	});
 	
 	res.render('index', { title: 'Express' });
 });
