675ca9e5b35212d50866034d500bedcf977d294e
Chris Martin <cj@cjmart.in>
2014-06-05 21:37:29 -0700

    ignore duplicate reads

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var nfc = require('nfc').nfc,
-    mifare = require('mifare-classic'),
-    ndef = require('ndef');
+	mifare = require('mifare-classic'),
+	ndef = require('ndef');
 
 var app = express();
 
@@ -30,9 +30,9 @@ app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 /// error handlers
@@ -40,29 +40,36 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 var n = new nfc();
+var lastUID = '';
 
 n.on('uid', function(uid) {
-    console.log('UID:', uid);
+	uid = uid.toString('hex');
+	if (UID == uid) {
+		return;
+	} else {
+		UID = uid;
+		console.log('UID:', uid);
+	}
 });
 
 n.start();
